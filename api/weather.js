0b039a30a626877f725f57d6930807b596fd7b79
Tokoiti <principal@tokoiti.school.nz>
2025-04-26 19:36:15 +1200

    Change api call

--- api/weather.js
+++ api/weather.js
@@ -5,18 +5,18 @@ export default async function handler(req, res) {
   const lat = '-46.1213'; // Tokoiti School latitude
   const lon = '169.9609'; // Tokoiti School longitude
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,daily,alerts&units=metric&appid=${apiKey}`;
 
   try {
     const response = await axios.get(url);
     const data = response.data;
 
     const result = {
-      temp: Math.round(data.main.temp),
-      description: data.weather[0].main,
-      uvIndex: "N/A", // Not available in this simple endpoint
-      pop: "N/A",     // Not available in this simple endpoint
-      icon: data.weather[0].icon
+      temp: Math.round(data.current.temp),
+      description: data.current.weather[0].main,
+      uvIndex: data.current.uvi,
+      pop: Math.round((data.hourly[0].pop || 0) * 100),
+      icon: data.current.weather[0].icon
     };
 
     res.status(200).json(result);
